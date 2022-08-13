fded1715f3b0110d3795651d7758fa4a50600699
KishanModi <kishandmodi@gmail.com>
2022-08-13 16:08:59 +0530

    added cors

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/', function (req, res) {
 app.get('/get', function (req, res) {
 	let city = req.query.city;
 	console.log(city);
-	let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+	let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=matric&appid=${apiKey}`;
 
 	request(url, function (err, response, body) {
 		if (err) {

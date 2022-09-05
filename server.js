a086aa9fb3cba580b4040e49abbb307d99cf7c69
KishanModi <kishandmodi@gmail.com>
2022-09-05 17:11:48 +0530

    added torrent support

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 const PORT = process.env.PORT || '8080';
-
+const axios = require('axios');
 const cors = require('cors');
 const data = require('./cities.json');
 const movies = require('./movies.json');
@@ -25,6 +25,27 @@ const apiKey = '4fd95260ee4e78352fea592ff624e953';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const getTorrents = async (query) => {
+    try {
+        const links = await axios.get(
+            `https://tscrap.herokuapp.com/torrents?key=${query}`
+        );
+
+        if (links.data.length === 0) {
+            return { data: '', success: false };
+        }
+        const magnet = await axios.get(
+            `https://tscrap.herokuapp.com/magnet?link=${links.data[0].url}`
+        );
+        if (Object.keys(magnet.data).length === 0) {
+            return { data: '', success: false };
+        } else {
+            return { data: magnet.data['magnet'], success: true };
+        }
+    } catch (err) {
+        return { data: '', success: false };
+    }
+};
 app.get('/', function (req, res) {
     res.send({ weather: null, error: null, code: 200 });
 });
@@ -98,6 +119,13 @@ app.get('/movies', (req, res) => {
         data: filteredMovies.slice(0, 10),
         success: true
     });
+});
+
+app.get('/torrents', async (req, res) => {
+    const params = req.query.q;
+    const result = await getTorrents(params);
+    console.log(result);
+    res.send(result);
 });
 app.listen(PORT, () => {
     console.log('App is running on port ' + PORT);
